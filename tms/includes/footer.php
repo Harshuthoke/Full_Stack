e473b020cfeb3f4bd6b00a7437f8d54e288006bc
Harshuthoke <harshuthoke@gmail.com>
2024-02-02 20:43:45 +0530

    first commit

---
+++ tms/includes/footer.php
@@ -0,0 +1,20 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="https://www.facebook.com/MITWPUOfficial/"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="https://twitter.com/MitWpu"><span>Twitter</span></a></li>				
+				<li><a class="googleplus" href="https://mitwpu.edu.in/"><span>Google+</span></a></li>
+				
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2023 XploreMH . All Rights Reserved </p>
+	</div>
+</div>
